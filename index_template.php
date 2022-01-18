963458ccab69dfc90b0ac591c90fd7b1127b8d89
Seanydegen <45349066+Seanydegen@users.noreply.github.com>
2022-01-18 15:31:13 +0800

    Start of Template

---
+++ index_template.php
@@ -0,0 +1,21 @@
+<htmml>
+    <head>
+        <title>Shoes Fever</title>
+        <link rel= "stylesheet" href="CSS/index_template.css" type="text/css"/>
+    </head>
+    <header>
+        <div class="container">
+        <img src ="source/images/header_logo.jpg" alt="logo" class="logo">
+             <nav>
+                <ul class = "menu">
+                    <li><a class="button" href="index.html">Home</a></li>
+                    <li><a class="button" href="index.html">Buy - View Listing</a></li>
+                    <li><a class="Button" href="index.html">Sell</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <body>
+
+    </body>
+</html>
