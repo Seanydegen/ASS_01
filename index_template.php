f2a6cab18efedd13b6915e95b258f2326483e063
Seanydegen <45349066+Seanydegen@users.noreply.github.com>
2022-01-19 12:52:10 +0800

    Template for Navigation Bar & Logo is completed

--- index_template.php
+++ index_template.php
@@ -1,21 +1,28 @@
-<htmml>
+<!--Please note that this is just a template for the Navigation Bar and Logo. Please make another copy of this file and rename it. 
+Then, add the following HTML & PHP codes wherever you see fit. :) -->
+<!DOCTYPE html>
+<html>
     <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <title>Shoes Fever</title>
-        <link rel= "stylesheet" href="CSS/index_template.css" type="text/css"/>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1"><!--Please remember to change stlye sheet for each page-->
+        <link rel= "stylesheet" href="styles/index_template.css" type="text/css"/> 
     </head>
-    <header>
-        <div class="container">
-        <img src ="source/images/header_logo.jpg" alt="logo" class="logo">
-             <nav>
-                <ul class = "menu">
-                    <li><a class="button" href="index.html">Home</a></li>
-                    <li><a class="button" href="index.html">Buy - View Listing</a></li>
-                    <li><a class="Button" href="index.html">Sell</a></li>
-                </ul>
-            </nav>
-        </div>
-    </header>
     <body>
-
+        <header>
+            <img class="logo" src ="source/images/nav_logo.png" alt="logo">
+                <nav>
+                <ul class ="nav__links">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">View Listings</a></li>
+                    <li><a href="#">Sell Your Shoes</a></li>
+                    <li><a href="#">Contact Us</a></li> <!--This contact us page is stated here just for show. If we have time we can create a simple form that doesn't work-->
+                </ul>
+                </nav>
+        </header>
     </body>
 </html>
+
+
---
+++ readme.txt
@@ -0,0 +1,5 @@
+color-palette used:https://colorhunt.co/palette/f5f5f5f0545430475e121212
+
+Instructions:
+Using the index_template.php & index_template.css. Create your own PHP and & CSS using these templates for the navigation bar and logo.
+Thank You :) If you need the color palette for fonts and background color is at the top ^.
