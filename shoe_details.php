0cea42aceb78e7fa02df6d67c2796f6db7489576
Seanydegen <45349066+Seanydegen@users.noreply.github.com>
2022-01-20 15:23:49 +0800

    Grid System Images and Added Express Interest Count

--- shoe_details.php
+++ shoe_details.php
@@ -25,36 +25,55 @@
         <!--------------Product Details -------------->
         <div class="small-cointainer shoe_details">
             <div class="row">
-                <div class="col-2">
-                    <img src="source/shoe_gallery/shoe_1.jfif" >
-                </div>
-                <div class="col-2">
-                    <?php 
-                    /*----------------------Test Case-----------------------------------*/
-                    $productno = date("d-m-y")."-abc";
-                    $productname = " Yellow Nike Running Shoes";
-                    $productprice = 35.00;
-                    $productshoetype = " Running Shoes";
-                    $productcolor = " Yellow";
-                    $productsize = " US 9.5";
-                    $productcondition = " Worn";
-                    $productbrand = " Nike";
-                    $productdesc = " This shoe has been heavily worn for 3 years";
-                    
+                <section id="photoArray">
+                    <div class="col-2">
+                        <img src="source/shoe_gallery/shoe_1.jfif" width="600px" height="600">
+                    </div>
+                    <div class="col-2">
+                        <img src="source/shoe_gallery/shoe_2.jfif" width="600px" height="600">
+                    </div>
+                    <div class="col-2">
+                        <img src="source/shoe_gallery/shoe_3.jfif" width="600px" height="600">
+                    </div>
+                    <div class="col-2">
+                        <img src="source/shoe_gallery/shoe_4.jfif" width="600px" height="600">
+                    </div>
+                </section>
+                <section id=productdetails>
+                    <div class="col-2">
+                        <?php 
+                        /*----------------------Test Case-----------------------------------*/
+                        $productno = date(" d-m-y")."-abc";
+                        $productname = " Yellow Nike Running Shoes";
+                        $productprice = 35.00;
+                        $productshoetype = " Running Shoes";
+                        $productcolor = " Yellow";
+                        $productsize = " US 9.5";
+                        $productcondition = " Worn";
+                        $productbrand = " Nike";
+                        $productdesc = " This shoe has been heavily worn for 3 years";
+                        $interestcount = 70;
+                        
 
-                    /*Code to Read from Txt.File will be here*/
-                    /*Add product variable here*/ 
-                    echo "<h2>Product Name:". $productname ."</h2><br>";
-                    echo "<h4>Product Number:". $productno ."</h4><br>";
-                    echo "<h4>Shoe Type:". $productshoetype ."</h4><br>";
-                    echo "<h4>Color:". $productcolor ."</h4><br>";
-                    echo "<h4>Size (US):". $productsize ."</h4><br>";
-                    echo "<h4>Condition:". $productcondition ."</h4><br>";
-                    echo "<h4>Description:". $productdesc ."</h4><br>";
-                    echo "<h4>Price (S$):". "$". $productprice ."</h4><br>";
-                    ?>
-                    <a href="#" class ="btn">Express Interest</a>
-                </div>
+                        /*Code to Read from Txt.File will be here*/
+                        /*Add product variable here*/ 
+                        echo "<h2>Product Name:". $productname ."</h2><br>";
+                        echo "<h4>Product Number:". $productno ."</h4><br>";
+                        echo "<h4>Shoe Type:". $productshoetype ."</h4><br>";
+                        echo "<h4>Color:". $productcolor ."</h4><br>";
+                        echo "<h4>Size (US):". $productsize ."</h4><br>";
+                        echo "<h4>Condition:". $productcondition ."</h4><br>";
+                        echo "<h4>Description:". $productdesc ."</h4><br>";
+                        echo "<h4>Price (S$):". "$". $productprice ."</h4><br>";
+                        ?>
+                        <a href="#" class ="btn">Express Interest</a>
+                        <div class = "interest_count">
+                            <?php
+                                echo "<p>". $interestcount ." people are interested in this product right now.</p><br>";
+                            ?>
+                        </div>
+                    </div>
+                </section>
             </div>
         </div>
 
@@ -64,6 +83,7 @@
             <?php 
             echo "Date:". date(" d-m-y")."<br>";
             ?>
+            <h3>© Trademarked by Shoe's Fever</h3>
         </div>
     </body>
 </html>
