37fdd051210f370ab5ac6a51c95f39cba1c42505
Seanydegen <45349066+Seanydegen@users.noreply.github.com>
2022-02-03 14:41:20 +0800

    Removed Slider

--- index.php
+++ index.php
@@ -31,66 +31,9 @@
                 </ul>
                 </nav>
         </header>
-        
-        <!-- Slideshow container -->
-            <div class="slideshow-container">
-
-            <!-- Full-width images with number and caption text -->
-                <div class="mySlides fade">
-                    <img src="source/slider/skate.jpg" style="width:100%">
-                    <div class="text">Caption Text</div>
-                </div>
-
-            <div class="mySlides fade">
-                <img src="source/slider/basket.jpg" style="width:100%">
-                <div class="text">Caption Two</div>
+            <div class= "photo-contain">
+                <img class="photo" src ="source/slider/skate.jpg">
             </div>
-
-            <div class="mySlides fade">
-                <img src="source/slider/train.jpg" style="width:100%">
-                <div class="text">Caption Three</div>
-            </div>
-
-            <!-- Next and previous buttons -->
-            <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-            <a class="next" onclick="plusSlides(1)">&#10095;</a>
-            </div>
-            <br>
-
-            <!-- The dots/circles -->
-        <div style="text-align:center">
-            <span class="dot" onclick="currentSlide(1)"></span>
-            <span class="dot" onclick="currentSlide(2)"></span>
-            <span class="dot" onclick="currentSlide(3)"></span>
-        </div>
-        <script>
-        var slideIndex = 1;
-            showSlides(slideIndex);
-
-            function plusSlides(n) {
-            showSlides(slideIndex += n);
-            }
-
-            function currentSlide(n) {
-            showSlides(slideIndex = n);
-            }
-
-            function showSlides(n) {
-            var i;
-            var slides = document.getElementsByClassName("mySlides");
-            var dots = document.getElementsByClassName("dot");
-            if (n > slides.length) {slideIndex = 1}    
-            if (n < 1) {slideIndex = slides.length}
-            for (i = 0; i < slides.length; i++) {
-                slides[i].style.display = "none";  
-            }
-            for (i = 0; i < dots.length; i++) {
-                dots[i].className = dots[i].className.replace(" active", "");
-            }
-            slides[slideIndex-1].style.display = "block";  
-            dots[slideIndex-1].className += " active";
-            }
-        </script>
         <!-- Navigation Button-->
         <a href="shoe_list.php" class ="btn">View Our Shoes</a>
         <a href="create_listing.php" class ="btn">Create a New Listing</a>
